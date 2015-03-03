81316281c00a09689076a0b8d032ebc251ba7846
amnotz <am.notzz@gmail.com>
2015-03-04 03:12:35 +0700

    now i have delete some file

--- app/src/main/java/com/socialbike/phuketsocialbike/Biker_board.java
+++ app/src/main/java/com/socialbike/phuketsocialbike/Biker_board.java
@@ -8,5 +8,10 @@ public class Biker_board extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_biker_board);
+
+
+
+
+        
     }
 }
